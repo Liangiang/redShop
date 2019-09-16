f67395bbf3327b1da8fa0e11045a5396a4411a79
Lx <liangxiu@htah.com.cn>
2019-09-16 10:46:27 +0800

    新增redAdmin服务——解决启动自动结束问题（新增pom文件web，springboot-web-start）

---
+++ lxShop-redamin/src/main/java/com/lxshop/redamin/RedaminApplication.java
@@ -0,0 +1,13 @@
+package com.lxshop.redamin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.cloud.client.SpringCloudApplication;
+
+@SpringCloudApplication
+public class RedaminApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RedaminApplication.class, args);
+    }
+
+}
